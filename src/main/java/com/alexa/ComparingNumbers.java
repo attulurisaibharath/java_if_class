89757bc806cd967021da82f0f6cc78f1bb0c2809
bharathchowdary <Saibharath0459@gmail.com>
2023-05-22 20:02:39 -0500

    Basic comparision of two numbers

--- src/main/java/com/alexa/ComparingNumbers.java
+++ src/main/java/com/alexa/ComparingNumbers.java
@@ -9,10 +9,14 @@ public class ComparingNumbers {
         int num1= sc.nextInt();
         System.out.println("Enter another number:");
         int num2= sc.nextInt();
-        if(num1>num2){
-            System.out.println("The greatest of two numbers is :"+num1);
+        System.out.println("Enter a third number:");
+        int num3=sc.nextInt();
+        if(num1>num2 && num1>num3){
+            System.out.println("The greatest of three numbers is :"+num1);
+        }else if(num2>num3){
+            System.out.println("The greatest of three numbers is :"+num2);
         }else{
-            System.out.println("The greatest of two numbers is :"+num2);
+            System.out.println("The greatest of three numbers is:"+num3);
         }
     }
 }
