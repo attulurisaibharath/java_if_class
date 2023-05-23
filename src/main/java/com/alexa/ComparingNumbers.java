54737d625867575e72c6c58800f5c19742987dda
bharathchowdary <Saibharath0459@gmail.com>
2023-05-22 20:24:17 -0500

    Code has been corrected with necessary changes

--- src/main/java/com/alexa/ComparingNumbers.java
+++ src/main/java/com/alexa/ComparingNumbers.java
@@ -13,12 +13,10 @@ public class ComparingNumbers {
         int num3=sc.nextInt();
         if(num1>num2 && num1>num3){
             System.out.println("The greatest of three numbers is :"+num1);
-        }else if(num2>num3){
+        }else if(num2>num3 && num2==num3){
             System.out.println("The greatest of three numbers is :"+num2);
             
-        }else if(num1 == num2 || num2 == num3){
-            System.out.println("Two of the given numbers are equal");
-            
+        }
         else{
             System.out.println("The greatest of three numbers is:"+num3);
         }
