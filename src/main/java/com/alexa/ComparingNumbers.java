5ce0300f70c7b3dfcbe5348466ec9e3ed07c2db6
attulurisaibharath <130122865+attulurisaibharath@users.noreply.github.com>
2023-05-22 20:29:45 -0500

    Update ComparingNumbers.java
    Made a simplified code with changes

--- src/main/java/com/alexa/ComparingNumbers.java
+++ src/main/java/com/alexa/ComparingNumbers.java
@@ -13,10 +13,13 @@ public class ComparingNumbers {
         int num3=sc.nextInt();
         if(num1>num2 && num1>num3){
             System.out.println("The greatest of three numbers is :"+num1);
-        }else if(num2>num3 && num2==num3){
+        }else if(num2>num3 ){
             System.out.println("The greatest of three numbers is :"+num2);
             
+            }else if(num2==num3){
+             System.out.println("From the above input second and third number are equal and the greatest is:"+num2);
         }
+            
         else{
             System.out.println("The greatest of three numbers is:"+num3);
         }
